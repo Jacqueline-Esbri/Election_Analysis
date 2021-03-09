de068d1b7eb444538411f10a1ccc00649d181889
je8778 <jackie6578@gmail.com>
2021-03-09 08:59:39 -0500

    Section 3.4.5

--- PyPoll.py
+++ PyPoll.py
@@ -1,75 +1,39 @@
-# Add our dependencies.
+# The data we need to retrieve.
+# 1. The total numbers of votes cast
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote
+
+# Import the datetime class from the datetime module.
+import datetime
+# Use the now() attribute on the datetime class to get the present time.
+now = datetime.datetime.now()
+# Print the present time.
+print("The time right now is ", now)
+
+# Assign a variable for the file to load and the path.
+file_to_load = 'Resources/election_results.csv'
+# Open the election results and read the file.
+election_data = open(file_to_load, 'r')
+# Open the election results and read the file
+with open(file_to_load) as election_data:
+
+     # To do: perform analysis.
+     print(election_data)
+
+# Add your dependencies.
 import csv
 import os
 # Assign a variable to load a file from a path.
 file_to_load = os.path.join("Resources", "election_results.csv")
 # Assign a variable to save the file to a path.
 file_to_save = os.path.join("analysis", "election_analysis.txt")
-# Initialize a total vote counter.
-total_votes = 0
-# Candidate options and candidate votes.
-candidate_options = []
-candidate_votes = {}
-# Track the winning candidate, vote count, and percentage.
-winning_candidate = ""
-winning_count = 0
-winning_percentage = 0
+
 # Open the election results and read the file.
 with open(file_to_load) as election_data:
-     file_reader = csv.reader(election_data)
-     # Read the header row.
-     headers = next(file_reader)
-      # Print each row in the CSV file.
-    for row in file_reader:
-         # Add to the total vote count.
-         total_votes += 1
-        # Get the candidate name from each row.
-        candidate_name = row[2]
-        # If the candidate does not match any existing candiddate, add the 
-        # the candidate list.
-        if candidate_name not in candidate_options:
-             # Add the candidate name to the list.
-             candidate_options.append(candidate_name)
-             # And begin tracking that candidate's voter count.
-             candidate_votes[candidate_name] = 0
-          # Add a vote to that candidate's count.
-          candidate_votes[candidate_name] += 1
+    file_reader = csv.reader(election_data)
 
-     # Save the results to our next text file.
-     with open(file_to_save, "w") as text file:
-          # Print the final vote count to the terminal.
-          election_results = (
-               f'\nElection results\n"
-               f"                   \n
-               f"Total Votes: {total_votes:,}\n"
-               f"                            \n"
-          print(election_results, end="")
-          # Save the final vote count to the text file.
-          txt_file.write(election_results)
-          for candidate_name in candidate_votes:
-               # Retrieve vote count and percentage.
-               votes = candidate_votes[candidate_name]
-               vote_percentage = float(votes) / float(total_votes) * 100
-               candidate_results = (
-                    f"{candidate_name}: {vote_percentage :.1f}% ({votes:,})\n")
+    # Read and print the header row.
+    headers = next(file_reader)
+    print(headers)
 
-               # Print each candidate's voter count and percentage to the terminal.
-               print(candidate_results)
-               # Save the candiddate results to our text file.
-               txt.file.write(candidate_results)
-               # Determine winning vote count, winning percentage, and winning candidate.
-               if (votes > winning_count) and (vote_percentage > winning_percentage):
-                    winning_count = votes
-                    winning_candidate = candidate_name
-                    winning_percentage = vote_percentage
-          # print the winning candidate's results to the terminal.
-          winning_candidate_summary = (
-               f"                   \n"
-               f"Winner: {winning_candidate}\n"
-               f"Winning Vote Count: {winning_count:,}\n"
-               f"Winning percentage: {winning_percentage:.1f}%\n"
-               f"                     /n"
-          print(winning_candidate_summary)
-          # Save the winning candidate's results to the text file.
-          txt.write(winning_candidate-summary)
-          
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Election_Analysis
+
+## Project Overview
+
+For this project, we were given a task to assist Tom, a Board of Elections employee, tabulate and audit the results of an election. During this assignment, we were introduced to Python; the most widely used programming language and a very powerful tool. Python efficiently enabled the process of writing algorithms to confirm and analyze the election results. This programming language has risen in popularity due to its simple structure, it is easy to read and maintain, and has a broad standard library.
+
+Python was not the only tool used to execute the scripts. In order to edit and save the code as it was being written, we use another great tool: Visual Studio Code.
+This free text editor is the most popular developer tool. It is also widely used by millions of programmers aroung the world. Having these two programs working in conjuction to analyze the data made the workflow faster, and increased productivity. The task that were required through the use of Python and VS Code were the following:
+- Calculating the total number votes
+- Total of votes that candidates received
+- Percentage of the votes that the candidates received
+- Winner based on popular vote
+
+## Resources
+
+The data provided to perform this analysis was a CSV file, which could quickly be converted to an Excel or Numbers file. The python version required was 3.7.6, but version 3.8.5 was a better choice for security reasons. The version of VS Code used was 2.3.0. 
+
+## Summary
+
+The analysis of the data from the election showed the following:
